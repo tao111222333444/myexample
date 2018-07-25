9e02953da70a4fa385f4fc9c90737ad1cc6b7ccf
loves_ <297947829@qq.com>
2018-07-25 16:24:50 +0800

    提交 关于反射的代码  和一个工厂模式

--- myreflection/src/main/java/com/hugo/myreflection/factory/Factory.java
+++ myreflection/src/main/java/com/hugo/myreflection/factory/Factory.java
@@ -11,6 +11,7 @@ public class Factory {
     public static Product getInstance(Class aa){
         Product concreteProduct = null;
         try {
+            // 通过Class对象动态创建该产品类的实例
             concreteProduct = (Product) aa.newInstance();
         } catch (InstantiationException e) {
             e.printStackTrace();
@@ -23,7 +24,9 @@ public class Factory {
     public static Product getInstance(String  ClassName){
         Product concreteProduct = null;
         try {
+            // 1. 根据 传入的产品类名 获取 产品类类型的Class对象
             Class productClass = Class.forName(ClassName);
+            // 2. 通过Class对象动态创建该产品类的实例
             concreteProduct = (Product) productClass.newInstance();
         } catch (InstantiationException e) {
             e.printStackTrace();
